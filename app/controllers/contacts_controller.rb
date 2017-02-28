96487869dda75a8cdf19096c8a5e0f938f608cca
JP S <jpsinghis@khalsa.com>
2017-02-28 07:03:48 +0000

    Added Hirb gem for data presentation

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,10 +1,10 @@
 class ContactsController < ApplicationController
   def new
-    @contact = Contact.new
+    @contact = Contact.new #{name: , email: , comments: }
   end
   
   def create
-    @contact = Contact.new(contact_params)
+    @contact = Contact.new(contact_params) #{name: 'person', email: 'person@email.com', comments: 'something tasteful'} This mass assingment is formed by 'contact_params' and the values are stored in the blank Contact.new, which is probably stored in the instanced variable, '@contact'.
     # @variables are called instance variables | This point to new.html.erb
     # This will create
     if @contact.save

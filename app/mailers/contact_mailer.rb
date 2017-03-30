2464ca5a15d65285cf2bae893201f74f9fa782f0
JP S <jpsinghis@khalsa.com>
2017-03-30 01:00:31 +0000

    minor change

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default to: 'verew@vps911.net'
+  default to: 'jpsinghis@khalsa.com'
   def contact_email(name, email, body)
     @name = name
     @email = email
